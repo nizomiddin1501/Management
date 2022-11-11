367590a0cc36f3c5ab6dba0aae0e7d0de4385ee8
Nizomiddin <nizomiddinmirzanazarov@gmail.com>
2022-11-12 02:10:04 +0500

    first

---
+++ src/main/java/uz/developers/management/security/JwtProvider.java
@@ -0,0 +1,4 @@
+package uz.developers.management.security;
+
+public class JwtProvider {
+}
---
+++ src/test/java/uz/developers/management/HrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.developers.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
